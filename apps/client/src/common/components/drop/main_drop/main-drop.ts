b98fe0c2b97429840a1e84e706403aacc9631419
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-13 18:11:58 +0200

    🪾 header_sorter => 🛠️ added autoclose on mouse out

--- apps/client/src/common/components/drop/main_drop/main-drop.ts
+++ apps/client/src/common/components/drop/main_drop/main-drop.ts
@@ -1,10 +1,19 @@
 import { SvgT } from '@/common/types/etc';
 import { NgClass, NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  HostListener,
+  inject,
+  Signal,
+  ViewChild,
+} from '@angular/core';
 import { SvgStrokeIconArrowDown } from '../../svgs/stroke/icon-arrow-down/icon-arrow-down';
 import { SortersUiFkt, SorterT } from '@/core/ui_fkt/etc/sorters';
 import { SvgStrokeIconCheck } from '../../svgs/stroke/icon-check/icon-check';
 import { UseDropHk } from '@/core/hooks/use_drop';
+import { ElDomT, RefDomT } from '@/common/types/dom';
 
 @Component({
   selector: 'app-main-drop',
@@ -29,10 +38,23 @@ export class MainDrop {
     this.useDrop.isOpen.set(!this.useDrop.isOpen());
   }
 
+  @ViewChild('dropRef')
+  private readonly dropRef: RefDomT;
+
   // ? derived
   public readonly twd: Signal<string> = computed(() =>
     this.useDrop.isOpen()
       ? 'translate-y-0 opacity-1 pointer-events-auto'
       : '-translate-y-[50px] opacity-0 pointer-events-none'
   );
+
+  @HostListener('document:mousedown', ['$event'])
+  public onMouseDown(e: Event): void {
+    const drop: ElDomT = this.dropRef?.nativeElement;
+    const target: Node = e.target as Node;
+
+    if (!drop || !target) return;
+
+    if (this.useDrop.isOpen() && !drop.contains(target)) this.useDrop.isOpen.set(false);
+  }
 }
--- apps/client/src/layout/sidebar_mobile/sidebar-mobile.ts
+++ apps/client/src/layout/sidebar_mobile/sidebar-mobile.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  HostListener,
+  inject,
+  Signal,
+  ViewChild,
+} from '@angular/core';
 import { BlackBg } from '../black_bg/black-bg';
 import { SidebarMobileSlice } from '@/features/sidebar_mobile/slice';
 import { NgClass } from '@angular/common';
 import { FilterRoadmapT, FiltersUiFkt } from '@/core/ui_fkt/etc/filters';
 import { PairValLabelT } from '@/common/types/forms';
+import { ElDomT, RefDomT } from '@/common/types/dom';
 
 @Component({
   selector: 'app-sidebar-mobile',
@@ -14,6 +23,9 @@ import { PairValLabelT } from '@/common/types/forms';
 })
 export class SidebarMobile {
   private readonly sideSlice: SidebarMobileSlice = inject(SidebarMobileSlice);
+
+  @ViewChild('sideRef')
+  private readonly sideRef: RefDomT;
 
   // ? derived
   public readonly blackBgCSS: Signal<string> = computed(
@@ -26,4 +38,14 @@ export class SidebarMobile {
   // ? static
   public readonly filtersFeedback: PairValLabelT[] = FiltersUiFkt.filtersFeedback();
   public readonly filtersRoadmap: FilterRoadmapT[] = FiltersUiFkt.filtersRoadmap();
+
+  @HostListener('document:mousedown', ['$event'])
+  public onMouseDown(e: Event): void {
+    const side: ElDomT = this.sideRef?.nativeElement;
+    const target: Node = e.target as Node;
+
+    if (!side || !target) return;
+
+    if (this.sideSlice.isOpen() && !side.contains(target)) this.sideSlice.setIsOpen(false);
+  }
 }
