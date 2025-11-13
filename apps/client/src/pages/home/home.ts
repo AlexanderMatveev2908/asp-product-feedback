7dc143c934798e7c7d6d49fe64c5cdf8c5018eaf
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-13 09:52:07 +0200

    🪾 css_variables => 🛠️ _

--- apps/client/src/pages/home/home.ts
+++ apps/client/src/pages/home/home.ts
@@ -1,15 +1,10 @@
-import { SvgAdvIconNewFeedback } from '@/common/components/svgs/advanced/icon-new-feedback/icon-new-feedback';
-import { SvgT } from '@/common/types/etc';
-import { NgComponentOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [NgComponentOutlet],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {
-  public readonly Curr: SvgT = SvgAdvIconNewFeedback;
-}
+export class Home {}
