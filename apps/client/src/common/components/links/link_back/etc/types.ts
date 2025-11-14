e9c15185e68f863cc571a4d6121fd957e6e65e63
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-14 20:40:21 +0200

    🪾 road_map => 🛠️ built header roadmap

---
+++ apps/client/src/common/components/links/link_back/etc/types.ts
@@ -0,0 +1,1 @@
+export type LinkBackVersionT = 'a' | 'b';
--- apps/client/src/pages/products/read/roadmap/products-roadmap.ts
+++ apps/client/src/pages/products/read/roadmap/products-roadmap.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LinkBack } from '@/common/components/links/link_back/link-back';
+import { LinkMain } from '@/common/components/links/link_main/link-main';
+import { UseMetaAppDir } from '@/core/directives/use_meta_app';
 
 @Component({
   selector: 'app-products-roadmap',
-  imports: [],
+  imports: [LinkBack, LinkMain, UseMetaAppDir],
   templateUrl: './products-roadmap.html',
   styleUrl: './products-roadmap.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
