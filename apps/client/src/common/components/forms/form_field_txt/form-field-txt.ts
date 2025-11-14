6be46e7674dcae77c41c616acca5029ba498027a
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-14 19:34:23 +0200

    🪾 post_feedback => 🛠️ _

--- apps/client/src/common/components/forms/form_field_txt/form-field-txt.ts
+++ apps/client/src/common/components/forms/form_field_txt/form-field-txt.ts
@@ -1,6 +1,7 @@
 import { TxtFieldT } from '@/common/types/forms';
 import { UseInjCtxHk } from '@/core/hooks/use_inj_ctx';
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   inject,
@@ -13,6 +14,7 @@ import { FormHeaderField } from '../form_header_field/form-header-field';
 import { UseFormFieldDir } from '@/core/directives/use_form_field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormErr } from '../form_err/form-err';
+import { FocusDom } from '@/core/lib/dom/focus';
 
 @Component({
   selector: 'app-form-field-txt',
@@ -22,9 +24,10 @@ import { FormErr } from '../form_err/form-err';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [UseFocusHk],
 })
-export class FormFieldTxt extends UseInjCtxHk implements OnInit {
+export class FormFieldTxt extends UseInjCtxHk implements OnInit, AfterViewInit {
   // ? props
   public readonly f: InputSignal<TxtFieldT> = input.required();
+  public readonly focusOnMount: InputSignal<boolean> = input(false);
 
   // ? hooks
   public readonly useFocus: UseFocusHk = inject(UseFocusHk);
@@ -35,5 +38,13 @@ export class FormFieldTxt extends UseInjCtxHk implements OnInit {
   ngOnInit(): void {
     this.useFormField.setupState();
     this.useFormField.patchState();
+  }
+
+  ngAfterViewInit(): void {
+    this.useDOM(() => {
+      if (!this.focusOnMount()) return;
+
+      FocusDom.focusByDataField(this.f().field);
+    });
   }
 }
