76631ecef6abede0fbde8b8b81e10ee5fa50ff4e
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-16 19:53:17 +0200

    🪾 main => 🛠️ fix PMD issue

--- apps/server/app/src/main/java/server/lib/data_structure/LibMemory.java
+++ apps/server/app/src/main/java/server/lib/data_structure/LibMemory.java
@@ -16,6 +16,35 @@ public class LibMemory {
             Map.Entry::getValue, (oldVal, newVal) -> newVal, Dict::new));
   }
 
+  private static final Object cpyArray(Object val) {
+    int len = Array.getLength(val);
+    final Object newArr = Array.newInstance(val.getClass().getComponentType(), len);
+    for (int i = 0; i < len; i++)
+      Array.set(newArr, i, deepCpy(Array.get(val, i)));
+    return newArr;
+  }
+
+  private static final List<?> cpyList(List<?> list) {
+    final List<Object> newList = new ArrayList<>(list.size());
+    for (final Object item : list)
+      newList.add(deepCpy(item));
+    return newList;
+  }
+
+  private static final Map<?, ?> cpyMap(Map<?, ?> m) {
+    final Dict newMap = new Dict();
+    m.forEach((k, v) -> newMap.put(k.toString(), deepCpy(v)));
+    return newMap;
+  }
+
+  private static final boolean isPrimitive(Object val) {
+    return val instanceof String
+        || val instanceof Number
+        || val instanceof Boolean
+        || val instanceof Character
+        || val instanceof Enum<?>;
+  }
+
   public static final Object deepCpy(Object val) {
     if (val == null)
       return null;
@@ -23,37 +52,21 @@ public class LibMemory {
     if (val instanceof Dict d)
       return d.cpy();
 
-    if (val instanceof Map<?, ?> m) {
-      Dict newMap = new Dict();
-      m.forEach((k, v) -> newMap.put(k.toString(), deepCpy(v)));
-      return newMap;
-    }
+    if (val instanceof Map<?, ?> m)
+      return cpyMap(m);
 
-    if (val instanceof List<?> list) {
-      List<Object> newList = new ArrayList<>(list.size());
-      for (Object item : list)
-        newList.add(deepCpy(item));
-      return newList;
-    }
+    if (val instanceof final List<?> list)
+      return cpyList(list);
 
-    if (val.getClass().isArray()) {
-      int len = Array.getLength(val);
-      Object newArr = Array.newInstance(val.getClass().getComponentType(), len);
-      for (int i = 0; i < len; i++)
-        Array.set(newArr, i, deepCpy(Array.get(val, i)));
-      return newArr;
-    }
+    if (val.getClass().isArray())
+      return cpyArray(val);
 
-    if (val instanceof String
-        || val instanceof Number
-        || val instanceof Boolean
-        || val instanceof Character
-        || val instanceof Enum<?>)
+    if (isPrimitive(val))
       return val;
 
     if (val instanceof Cloneable) {
       try {
-        Method cloneMethod = val.getClass().getMethod("clone");
+        final Method cloneMethod = val.getClass().getMethod("clone");
         return cloneMethod.invoke(val);
       } catch (Exception ignored) {
       }
