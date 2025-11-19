dbbc42ba39db7c02773e2c55107f82fe02108f77
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-19 13:00:05 +0200

    🪾 feedbacks_endpoints => 🛠️ finished big query maybe

--- __.sql
+++ __.sql
@@ -1,28 +1,3 @@
-package server.features.feedbacks.services;
-
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
-import server.conf.databases.relational_database.SqlCmd;
-import server.decorators.core.api.Api;
-import server.decorators.types.Dict;
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-@SuppressFBWarnings({ "EI2", "EI" })
-public class FeedReadAllSvc {
-  private final SqlCmd sqlCmd;
-
-  private Mono<List<Dict>> query() {
-    return sqlCmd.trxLowLevel(client -> {
-      StringBuilder sql = new StringBuilder();
-      sql.append(String.format("""
           SELECT f.*,
             COALESCE(
               (
@@ -105,13 +80,3 @@ public class FeedReadAllSvc {
               ,'[]'::JSON
             ) comments
           FROM feedbacks f;
-                    """));
-
-      return client.sql(sql.toString()).fetch().all().map(row -> Dict.fromRow(row)).collectList();
-    });
-  }
-
-  public Mono<Dict> main(Api api) {
-    return query().map(list -> Dict.of("feedbacks", list));
-  }
-}
