633765a2bc8cedd561685ba5b98b396cd2ab79aa
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-20 09:46:27 +0200

    🪾 filter_feedbacks => 🛠️ added to home feedbacks signal as => DERIVED VALU

--- apps/client/src/features/feedbacks/etc/context/search_feedbacks_ctx.ts
+++ apps/client/src/features/feedbacks/etc/context/search_feedbacks_ctx.ts
@@ -1,10 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { SearchFeedbacksFormMng } from '../forms/search_feedbacks/form_mng';
+import { SearchFeedbacksFormMng, SearchFeedbacksFormT } from '../forms/search_feedbacks/form_mng';
+import { UseInjCtxHk } from '@/core/hooks/use_inj_ctx';
+import { Nullable } from '@/common/types/etc';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
-export class SearchFeedbacksCtx {
+export class SearchFeedbacksCtx extends UseInjCtxHk {
   public readonly form: FormGroup = SearchFeedbacksFormMng.form;
+
+  public formData: Nullable<Signal<SearchFeedbacksFormT>> = null;
+
+  public setupForm(): void {
+    this.inCtx(() => {
+      this.formData = toSignal(this.form.valueChanges, {
+        initialValue: this.form.value,
+      });
+    });
+  }
 }
--- apps/client/src/pages/home/home.ts
+++ apps/client/src/pages/home/home.ts
@@ -1,7 +1,14 @@
-import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  Signal,
+} from '@angular/core';
 import { PageWrapper } from '@/layout/page_wrapper/page-wrapper';
 import { MainDrop } from '@/common/components/drop/main_drop/main-drop';
-import { Nullable } from '@/common/types/etc';
+import { Nullable, OrNone } from '@/common/types/etc';
 import { FeedbacksSlice } from '@/features/feedbacks/slice';
 import { FeedbackT } from '@/features/feedbacks/etc/types';
 import { HomeNoData } from './home_no_data/home-no-data';
@@ -10,6 +17,7 @@ import { SidebarMobile } from '@/layout/sidebar_mobile/sidebar-mobile';
 import { UseMetaAppDir } from '@/core/directives/use_meta_app';
 import { LinkMain } from '@/common/components/links/link_main/link-main';
 import { FeedbackItem } from '@/features/feedbacks/etc/components/feedback_item/feedback-item';
+import { SearchFeedbacksCtx } from '@/features/feedbacks/etc/context/search_feedbacks_ctx';
 
 @Component({
   selector: 'app-home',
@@ -27,9 +35,27 @@ import { FeedbackItem } from '@/features/feedbacks/etc/components/feedback_item/
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {
-  private readonly productsSLice: FeedbacksSlice = inject(FeedbacksSlice);
+export class Home implements OnInit {
+  private readonly feedbacksSlice: FeedbacksSlice = inject(FeedbacksSlice);
+  private readonly searchCtx: SearchFeedbacksCtx = inject(SearchFeedbacksCtx);
 
-  public readonly isPending: Signal<boolean> = this.productsSLice.isPending;
-  public readonly feedbacks: Signal<Nullable<FeedbackT[]>> = this.productsSLice.feedbacks;
+  public readonly isPending: Signal<boolean> = this.feedbacksSlice.isPending;
+  public readonly feedbacks: Signal<Nullable<FeedbackT[]>> = this.feedbacksSlice.feedbacks;
+
+  // ? derived
+
+  public readonly filtered: Signal<Nullable<FeedbackT[]>> = computed(() =>
+    !this.feedbacks()
+      ? []
+      : this.feedbacks()!.filter((f: FeedbackT) => {
+          const filters: OrNone<string[]> = this.searchCtx.formData?.()?.category;
+          if (!filters || filters.includes('ALL')) return true;
+
+          return filters.some((str: string) => str === f.category);
+        })
+  );
+
+  ngOnInit(): void {
+    this.searchCtx.setupForm();
+  }
 }
