3ba56a08482fa4002804d2286fda8b29d17769b8
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-20 18:22:06 +0200

    🪾 add_feedback_endpoint => 🛠️ reminder => started PostFeedFormT to validated input data to post new feedback

---
+++ apps/server/app/src/main/java/server/features/feedbacks/paperwork/PostFeedFormT.java
@@ -0,0 +1,12 @@
+package server.features.feedbacks.paperwork;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class PostFeedFormT {
+  // @NotBlank(message = "title required")
+  // @Pattern(regexp = )
+
+}
