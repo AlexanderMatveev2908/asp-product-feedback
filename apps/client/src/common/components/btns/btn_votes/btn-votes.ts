6bd47a06a11bf376076d8dfa0b8cb62f8c6375c1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-20 15:06:14 +0200

    🪾 optimistic_like => 🛠️ added animations upvotes btn

--- apps/client/src/common/components/btns/btn_votes/btn-votes.ts
+++ apps/client/src/common/components/btns/btn_votes/btn-votes.ts
@@ -1,6 +1,13 @@
 import { SvgT } from '@/common/types/etc';
 import { NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+  InputSignal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { SvgStrokeIconArrowUp } from '../../svgs/stroke/icon-arrow-up/icon-arrow-up';
 
 @Component({
@@ -13,5 +20,16 @@ import { SvgStrokeIconArrowUp } from '../../svgs/stroke/icon-arrow-up/icon-arrow
 export class BtnVotes {
   public readonly upvotes: InputSignal<number> = input.required();
 
+  // ? local state
+  public readonly isFocused: WritableSignal<boolean> = signal(false);
+
+  public onFocus(): void {
+    this.isFocused.set(true);
+  }
+  public onBlur(): void {
+    this.isFocused.set(false);
+  }
+
+  // ? statics
   public readonly Chevron: SvgT = SvgStrokeIconArrowUp;
 }
