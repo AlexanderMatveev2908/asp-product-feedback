84a5f9c8b5e0b23998a24379ab1f0fa2f65bec82
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-21 15:23:05 +0200

    🪾 fix_back_links => 🛠️ _

--- apps/client/src/pages/feedbacks/read/:feedbackID/products-read.ts
+++ apps/client/src/pages/feedbacks/read/:feedbackID/products-read.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  Signal,
+} from '@angular/core';
 import { PageWrapper } from '@/layout/page_wrapper/page-wrapper';
 import { UseInjCtxHk } from '@/core/hooks/use_inj_ctx';
 import { BtnMain } from '@/common/components/btns/btn__main/btn-main';
@@ -9,10 +16,11 @@ import { FeedbackItem } from '@/features/feedbacks/etc/components/feedback_item/
 import { UseFindFeedByParams } from '@/core/hooks/use_find_feed_by_params';
 import { FeedbackT } from '@/features/feedbacks/etc/types';
 import { Nullable } from '@/common/types/etc';
+import { LinkMain } from '@/common/components/links/link_main/link-main';
 
 @Component({
   selector: 'app-products-read',
-  imports: [PageWrapper, BtnMain, CommentItem, UseMetaAppDir, LinkBack, FeedbackItem],
+  imports: [PageWrapper, BtnMain, CommentItem, UseMetaAppDir, LinkBack, FeedbackItem, LinkMain],
   templateUrl: './products-read.html',
   styleUrl: './products-read.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -22,6 +30,8 @@ export class ProductsRead extends UseInjCtxHk implements OnInit {
   private readonly useFindFeedByParams: UseFindFeedByParams = inject(UseFindFeedByParams);
 
   public readonly found: Signal<Nullable<FeedbackT>> = this.useFindFeedByParams.found;
+
+  public readonly pathEdit: Signal<string> = computed(() => `/feedbacks/put/${this.found()?.id}`);
 
   ngOnInit(): void {
     this.useFindFeedByParams.main();
