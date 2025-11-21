1dbae7fa1b3e214ff9d54c163cbe498378f460bd
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-21 11:05:27 +0200

    🪾 add_feedback_client => 🛠️ client form seem functional

--- apps/client/src/features/feedbacks/etc/forms/feedback_form/feedback-form.ts
+++ apps/client/src/features/feedbacks/etc/forms/feedback_form/feedback-form.ts
@@ -3,17 +3,26 @@ import { SvgAdvIconNewFeedback } from '@/common/components/svgs/advanced/icon-ne
 import { SvgT } from '@/common/types/etc';
 import { UseNavSvc } from '@/core/services/use_nav';
 import { NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  InputSignal,
+  Signal,
+} from '@angular/core';
 import { FeedbackFormFields, FeedbackFormUiFkt } from './etc/ui_fkt';
 import { FormFieldTxt } from '@/common/components/forms/form_field_txt/form-field-txt';
-import { FeedbackFormMng, FormKeyT } from './etc/form_mng';
+import { FeedbackFormMng, FeedbackFormPostT, FormKeyT } from './etc/form_mng';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UseFormFieldDir } from '@/core/directives/use_form_field';
 import { FormSelect } from '@/common/components/forms/form_select/form-select';
 import { BtnMain } from '@/common/components/btns/btn__main/btn-main';
 import { UseMetaAppDir } from '@/core/directives/use_meta_app';
 import { RootFormMng } from '@/core/paperwork/root_form_mng/root_form_mng';
-import { LibLog } from '@/core/lib/dev/log';
+import { Observable, tap } from 'rxjs';
+import { UseApiTrackerHk } from '@/core/store/api/etc/hooks/use_tracker';
 
 @Component({
   selector: 'app-feedback-form',
@@ -29,10 +38,20 @@ import { LibLog } from '@/core/lib/dev/log';
   templateUrl: './feedback-form.html',
   styleUrl: './feedback-form.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [UseApiTrackerHk],
 })
 export class FeedbackForm {
+  // ? props
+  public readonly strategy: InputSignal<(data: FeedbackFormPostT) => Observable<unknown>> =
+    input.required();
+
+  // ? svc
   private readonly useNav: UseNavSvc = inject(UseNavSvc);
 
+  // ? hooks
+  public readonly useApiTrack: UseApiTrackerHk = inject(UseApiTrackerHk);
+
+  // ? derived
   public readonly isFormTypePost: Signal<boolean> = computed(
     () => !!this.useNav.currPath()?.includes('post')
   );
@@ -44,19 +63,24 @@ export class FeedbackForm {
   );
 
   public readonly fields: FeedbackFormFields = FeedbackFormUiFkt.formFields();
-  public readonly formPost: FormGroup = FeedbackFormMng.formPost;
+  public readonly formPost: FormGroup = FeedbackFormMng.formPost();
 
+  // ? helpers
   public asFormControl(formKey: FormKeyT): FormControl {
     return this.formPost.get(formKey) as FormControl;
   }
 
+  // ? listeners
   public onSubmit(): void {
     if (!this.formPost.valid) {
       RootFormMng.onSubmitFailed(this.formPost);
       return;
     }
 
-    LibLog.logTtl('✅ ok', this.formPost.value);
+    this.useApiTrack
+      .track(this.strategy()(this.formPost.value))
+      .pipe(tap((_: unknown) => this.reset()))
+      .subscribe();
   }
 
   public reset: () => void = () => {
--- apps/client/src/pages/feedbacks/post/products-post.ts
+++ apps/client/src/pages/feedbacks/post/products-post.ts
@@ -1,7 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PageWrapper } from '@/layout/page_wrapper/page-wrapper';
 import { LinkBack } from '@/common/components/links/link_back/link-back';
 import { FeedbackForm } from '@/features/feedbacks/etc/forms/feedback_form/feedback-form';
+import { UseFeedKit } from '@/features/feedbacks/etc/services/use_feed_kit';
+import { FeedbackFormPostT } from '@/features/feedbacks/etc/forms/feedback_form/etc/form_mng';
+import { Observable, tap } from 'rxjs';
+import { ResApiT } from '@/core/store/api/etc/types';
+import { FeedbackT } from '@/features/feedbacks/etc/types';
+import { UseNavSvc } from '@/core/services/use_nav';
 
 @Component({
   selector: 'app-products-post',
@@ -10,4 +16,18 @@ import { FeedbackForm } from '@/features/feedbacks/etc/forms/feedback_form/feedb
   styleUrl: './products-post.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsPost {}
+export class ProductsPost {
+  private readonly useFeedKit: UseFeedKit = inject(UseFeedKit);
+  private readonly useNav: UseNavSvc = inject(UseNavSvc);
+
+  public readonly postStrategy: (data: FeedbackFormPostT) => Observable<unknown> = (
+    data: FeedbackFormPostT
+  ) =>
+    this.useFeedKit.api.post(data).pipe(
+      tap((_: ResApiT<{ feedback: FeedbackT }>) => {
+        this.useFeedKit.slice.refetch();
+
+        void this.useNav.replace('/');
+      })
+    );
+}
