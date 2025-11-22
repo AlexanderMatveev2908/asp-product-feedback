bd43db88967c5403ec4b82e775a3746ee453e598
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-22 13:41:53 +0200

    🪾 responsiveness => 🎨 _

--- apps/client/src/common/components/els/span/span.ts
+++ apps/client/src/common/components/els/span/span.ts
@@ -1,13 +1,5 @@
 import { NgComponentOutlet } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-  input,
-  InputSignal,
-  Signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, InputSignal } from '@angular/core';
 import { WrapTxtApi } from '../../hoc/txt/wrap_txt_api/wrap-txt-api';
 import { UseWrapApiDir } from '@/core/directives/use_wrap_api';
 import { UseSpanDir } from '@/core/directives/use_span';
@@ -27,15 +19,6 @@ export class Span {
 
   // ? props
   public readonly eventT: InputSignal<AppEventT> = input<AppEventT>('NONE');
-  public readonly spinSize: InputSignal<SpinTxtClsT> = input<SpinTxtClsT>('md');
+  public readonly spinSize: InputSignal<SpinTxtClsT> = input<SpinTxtClsT>('spin__sm');
   public readonly isPending: InputSignal<boolean> = input(false);
-
-  // ! custom classes only built at runtime
-  // ! tailwind would not support it
-  public txtCls: Signal<string> = computed(
-    () => `txt__${this.useSpanDir.spanSizesProps().txt ?? 'lg'}`
-  );
-  public svgCls: Signal<string> = computed(
-    () => `svg__${this.useSpanDir.spanSizesProps().svg ?? 'sm'}`
-  );
 }
--- apps/client/src/common/types/css.ts
+++ apps/client/src/common/types/css.ts
@@ -1,3 +1,22 @@
-export type TxtClsT = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
-export type SvgClsT = TxtClsT | '4xl' | '5xl';
-export type SpinTxtClsT = 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
+export type TxtClsT =
+  | 'txt__xxs'
+  | 'txt__xs'
+  | 'txt__sm'
+  | 'txt__md'
+  | 'txt__lg'
+  | 'txt__xl'
+  | 'txt__2xl'
+  | 'txt__3xl';
+export type SvgClsT =
+  | 'svg__xxs'
+  | 'svg__xs'
+  | 'svg__sm'
+  | 'svg__md'
+  | 'svg__lg'
+  | 'svg__lg'
+  | 'svg__xl'
+  | 'svg__2xl'
+  | 'svg__3xl'
+  | 'svg__4xl'
+  | 'svg__5xl';
+export type SpinTxtClsT = 'spin__xxs' | 'spin__xs' | 'spin__sm' | 'spin__md' | 'spin__lg';
