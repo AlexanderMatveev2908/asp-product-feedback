7812c1328b6962f43c0f0c38e0d0886835cdefe1
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-22 09:49:47 +0200

    🪾 reply_client => 🎨 _

---
+++ apps/server/app/src/test/resources/sql/migrations/down/005.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER IF EXISTS trigger_timestamp_feedbacks ON feedbacks;
+
+DROP TABLE IF EXISTS feedbacks;
+
+DROP TYPE IF EXISTS category_type;
+DROP TYPE IF EXISTS status_type;
---
+++ apps/server/app/src/test/resources/sql/migrations/down/007.sql
@@ -0,0 +1,3 @@
+DROP TRIGGER IF EXISTS trigger_timestamp_replies ON replies;
+
+DROP TABLE IF EXISTS replies;
