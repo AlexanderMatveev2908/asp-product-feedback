2af602c5938bbd200c92c8cf714a279e8fbd1daa
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-23 11:41:44 +0200

    🪾 responsiveness => 🛠️ _

--- apps/client/src/features/feedbacks/etc/components/feedback_item/feedback_content/version_col/card-version-col.ts
+++ apps/client/src/features/feedbacks/etc/components/feedback_item/feedback_content/version_col/card-version-col.ts
@@ -1,20 +1,8 @@
-import { SvgAdvIconComments } from '@/common/components/svgs/advanced/icon-comments/icon-comments';
-import { Nullable, SvgT } from '@/common/types/etc';
-import { UseNavSvc } from '@/core/services/use_nav';
-import { FeedLibShape } from '@/features/feedbacks/etc/lib_shape';
-import { FeedbackT } from '@/features/feedbacks/etc/types';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  inject,
-  input,
-  InputSignal,
-  Signal,
-} from '@angular/core';
-import { Params, RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { BtnVotes } from '@/common/components/btns/btn_votes/btn-votes';
 import { NgComponentOutlet, NgTemplateOutlet } from '@angular/common';
+import { UseCardVersioningDir } from '../directives/use_card_versioning';
 
 @Component({
   selector: 'app-card-version-col',
@@ -23,28 +11,4 @@ import { NgComponentOutlet, NgTemplateOutlet } from '@angular/common';
   styleUrl: './card-version-col.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VersionCol {
-  public readonly item: InputSignal<FeedbackT> = input.required();
-
-  private readonly useNav: UseNavSvc = inject(UseNavSvc);
-
-  public readonly footerWithLink: Signal<boolean> = computed(() => {
-    const path: Nullable<string> = this.useNav.currPath();
-    if (!path) return false;
-
-    const vars: Nullable<Params> = this.useNav.pathVariables();
-    return !vars?.['feedbackID'];
-  });
-
-  // ? statics
-  public readonly Comment: SvgT = SvgAdvIconComments;
-
-  // ? derived
-  public readonly pathComments: Signal<string> = computed(
-    () => `/feedbacks/read/${this.item()?.id}`
-  );
-
-  public readonly catLabel: Signal<Nullable<string>> = computed(() =>
-    FeedLibShape.catLabelByVal(this.item().category)
-  );
-}
+export class VersionCol extends UseCardVersioningDir {}
