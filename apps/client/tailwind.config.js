516ef1b695b22bc534ec2dc5fa64e38b04460a84
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-23 11:14:01 +0200

    🪾 responsiveness => 🛠️ _

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -20,7 +20,8 @@ export default {
         hd__xl: ['24px', { lineHeight: '35px', letterSpacing: '-0.33px', fontWeight: '700' }],
         hd__lg: ['20px', { lineHeight: '29px', letterSpacing: '-0.25px', fontWeight: '700' }],
         hd__md: ['18px', { lineHeight: '26px', letterSpacing: '-0.25px', fontWeight: '700' }],
-        hd__sm: ['14px', { lineHeight: '20px', letterSpacing: '-0.2px', fontWeight: '700' }],
+        hd__sm__bold: ['14px', { lineHeight: '20px', letterSpacing: '-0.2px', fontWeight: '700' }],
+        hd__sm__reg: ['14px', { lineHeight: '20px', letterSpacing: '-0.2px', fontWeight: '400' }],
 
         bd__lg__reg: ['16px', { lineHeight: '23px', fontWeight: '400' }],
         bd__lg__bold: ['16px', { lineHeight: '23px', fontWeight: '700' }],
