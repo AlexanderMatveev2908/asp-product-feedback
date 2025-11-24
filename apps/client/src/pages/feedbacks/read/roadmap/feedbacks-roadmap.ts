5708ecd62b55d03ea87f1dbe1ae195bc33fb780a
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-24 08:48:41 +0200

    🪾 responsiveness => 🛠️ _

--- apps/client/src/pages/feedbacks/read/roadmap/feedbacks-roadmap.ts
+++ apps/client/src/pages/feedbacks/read/roadmap/feedbacks-roadmap.ts
@@ -3,16 +3,14 @@ import { LinkBack } from '@/common/components/links/link_back/link-back';
 import { LinkMain } from '@/common/components/links/link_main/link-main';
 import { UseMetaAppDir } from '@/core/directives/use_meta_app';
 import { PageWrapper } from '@/layout/page_wrapper/page-wrapper';
-import { FeedbackStatusT } from '@/features/feedbacks/etc/types';
 import { FeedLibShape } from '@/features/feedbacks/etc/lib_shape';
-import { ErrApp } from '@/core/lib/etc/err';
-import { NgClass } from '@angular/common';
 import { UseRoadmapCtx } from '@/features/feedbacks/etc/context/use_roadmap_ctx';
 import { FeedbackBase } from '@/features/feedbacks/etc/components/feedback_item/feedback_base/feedback-base';
+import { MobileRoadHeader } from './etc/mobile_road_header/mobile-road-header';
 
 @Component({
   selector: 'app-feedbacks-roadmap',
-  imports: [LinkBack, LinkMain, UseMetaAppDir, PageWrapper, NgClass, FeedbackBase],
+  imports: [LinkBack, LinkMain, UseMetaAppDir, PageWrapper, FeedbackBase, MobileRoadHeader],
   templateUrl: './feedbacks-roadmap.html',
   styleUrl: './feedbacks-roadmap.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -29,28 +27,4 @@ export class FeedbacksRoadmap {
   public readonly currDescription: Signal<string> = computed(() =>
     FeedLibShape.descriptionStatusByVal(this.useRoadCtx.currStatus())
   );
-
-  public readonly twdHeaderLine: Signal<string> = computed(() => {
-    switch (this.useRoadCtx.currStatus()) {
-      case FeedbackStatusT.PLANNED:
-        return 'translate-x-0';
-      case FeedbackStatusT.IN_PROGRESS:
-        return 'translate-x-full';
-      case FeedbackStatusT.LIVE:
-        return 'translate-x-[200%]';
-      case FeedbackStatusT.SUGGESTION:
-        throw new ErrApp('suggestion should not be available as status');
-      default:
-        throw new ErrApp('invalid status type');
-    }
-  });
-
-  public bgHeaderLine(): string {
-    const clr: string = FeedLibShape.clrByStatus(this.useRoadCtx.currStatus());
-    return clr;
-  }
-
-  public twdLabel(v: FeedbackStatusT): string {
-    return this.useRoadCtx.currStatus() === v ? 'opacity-1' : 'opacity-[0.4]';
-  }
 }
