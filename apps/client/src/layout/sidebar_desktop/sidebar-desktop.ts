46335adb5e60bcef5efc1c887ab4a4a8a8e48d27
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-11-25 08:02:30 +0200

    🪾 desktop_rwd => 🛠️ _

--- apps/client/src/layout/sidebar_desktop/sidebar-desktop.ts
+++ apps/client/src/layout/sidebar_desktop/sidebar-desktop.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FeedbacksFilters } from '@/features/feedbacks/etc/components/feedbacks_filters/feedbacks-filters';
+import { FeedbacksRoadmapCounter } from '@/features/feedbacks/etc/components/feedbacks_roadmap_counter/feedbacks-roadmap-counter';
 
 @Component({
   selector: 'app-sidebar-desktop',
-  imports: [],
+  imports: [RouterLink, FeedbacksFilters, FeedbacksRoadmapCounter],
   templateUrl: './sidebar-desktop.html',
   styleUrl: './sidebar-desktop.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
